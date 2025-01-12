62bebfc84b807a5caf564b1aaa66b592b507307e
Dušan Simić <dusan.simic1810@gmail.com>
2025-01-12 03:21:34 +0100

    feat: Add eclipse build workflow

---
+++ scripts/build-eclipse.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -eoux pipefail
+
+rpmdev-setuptree
+
+mv /ctx/specs/eclipse/eclipse.desktop ~/rpmbuild/SOURCES
+mv /ctx/specs/eclipse/*.spec ~/rpmbuild/SPECS
+
+spectool -g -R ~/rpmbuild/SPECS/java.spec
+spectool -g -R ~/rpmbuild/SPECS/jee.spec
+ls -l ~/rpmbuild/SOURCES
+
+dnf --nodocs --assumeyes builddep ~/rpmbuild/SPECS/java.spec
+dnf --nodocs --assumeyes builddep ~/rpmbuild/SPECS/jee.spec
+
+rpmbuild -ba ~/rpmbuild/SPECS/java.spec
+rpmbuild -ba ~/rpmbuild/SPECS/jee.spec
